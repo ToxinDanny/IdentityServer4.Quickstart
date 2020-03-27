3f8f649389489956f45efbeac28d9cf7ae3e7abf
Daniele <daniele.tulli93@gmail.com>
2020-03-27 15:35:30 +0100

    Add project files.

---
+++ src/Client/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000"); 
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                throw new ArgumentNullException();
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:5001/identity");
+            
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(JArray.Parse(content));
+
+        }
+
+        //private static async Task<TokenResponse> GetToken(HttpClient client, DiscoveryDocumentResponse disco)
+        //{
+        //    var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+        //    {
+        //        Address = disco.TokenEndpoint,
+        //        ClientId = "client",
+        //        ClientSecret = "secret",
+        //        Scope = "api1"
+        //    });
+
+            
+        //    return tokenResponse;
+        //}
+    }
+}
---
+++ src/Mvc/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        public async Task<IActionResult> CallApi()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var content = await client.GetAsync("http://localhost:5001/Identity/Get");
+            ViewBag.Json = JArray.Parse(content.ReasonPhrase).ToString();
+            return View("json");
+        }
+    }
+}
